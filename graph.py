dc73929cfe0aa20b38c2aae2a8b904255e23eb79
Doug Tomm <dtomm@radiumone.com>
2017-03-19 10:13:56 -0700

    and so it begins

---
+++ graph.py
@@ -0,0 +1,192 @@
+import pprint
+import collections
+
+class GraphException(Exception):
+    pass
+
+class Node(object):
+
+    def __init__(self, label):
+        self._label = label
+
+    def __str__(self):
+        return '%s' % self._label
+
+    def __repr__(self):
+        return 'Node(%s)' % (self._label)
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            raise NotImplemented
+
+        if self._label != other._label:
+            return False
+
+        return True
+
+    def __hash__(self):
+        return hash(self._label)
+    
+    def show_ids(self, tag):
+        print '=' * 11, tag, '=' * 11
+        print repr(self)
+        print id(self)
+        print id(self._label)
+        
+    @property
+    def label(self):
+        return self._label
+    
+class Graph(object):
+
+    # implemented as a dictionary that maps nodes to sets
+    # of nodes.  the sets represent adjacent nodes.
+    # this is an undirected graph, so for an edge between two
+    # nodes, each appears as a key and is in the other's adjacency
+    # set.  because we are using a set, there can be at most one
+    # edge directly connecting two nodes.
+
+    def __init__(self):
+        self.adj_list = {}
+
+    def addNode(self, n):
+        if n not in self.adj_list:
+            self.adj_list[n] = set()
+
+    def contains(self, n):
+        return n in self.adj_list
+
+    def dump(self):
+        pp = pprint.PrettyPrinter()
+        
+        pp.pprint(self.adj_list)
+
+    def addEdge(self, a, b):
+        if not self.contains(a):
+            raise GraphException("node %s not in graph" % a)
+
+        if not self.contains(b):
+            raise GraphException("node %s not in graph" % b)
+
+        s = self.adj_list[a]
+        s.add(b)
+
+        s = self.adj_list[b]
+        s.add(a)
+        
+    '''
+    return the nodes in the graph, as a list
+    '''
+    def nodes(self):
+        return self.adj_list.keys()
+
+
+    def neighbors(self, n):
+        if not self.contains(n):
+            raise GraphException("node %s not in graph" % n)
+
+        sorted_neighbors = sorted(self.adj_list[n], key=lambda n: n._label)
+        for k in sorted_neighbors:
+            yield k
+
+##########    ##########    ##########    ##########    ##########
+
+def _next_unvisited_neighbor(g, n, visited):
+
+    adj_list = sorted(g.adj_list[n], key=lambda n: n._label)
+    for k in adj_list:
+        if k not in visited:
+            return k
+    return None
+
+def dfs(g, n):
+    # non-recursive implementation
+    
+    if not g.contains(n):
+        raise GraphException("node %s not in graph" % n)
+    
+    stack = []
+    visited = set()
+    
+    stack.append(n)
+    visited.add(n)
+    result = str(n)
+    
+    # look at node on top of stack
+    # if it has an unvisited neighbor,
+    # mark it visited and put it at top of stack
+    # otherwise pull it off
+    
+    while len(stack) > 0:
+        top = stack[-1]
+        # get next unvisited neighbor
+        k = _next_unvisited_neighbor(g, top, visited)
+        if k is None:
+            stack.pop()
+        else:
+            stack.append(k)
+            visited.add(k)
+            result += str(k)
+
+    return result
+            
+def bfs(g, n):
+    # non-recursive implementation
+
+    if not g.contains(n):
+        raise GraphException("node %s not in graph" % n)
+    
+    q = collections.deque()
+    visited = set()
+
+    # look at front of queue
+    # put all unvisited adj_list on queue, mark visited
+    # dequeue
+
+    q.append(n)
+    visited.add(n)
+    result = ""
+
+    while len(q) > 0:
+
+        front = q.popleft()
+        result += str(front)
+        
+        # enqueue all unvisited adj_list
+        adj_list = g.neighbors(front)
+        for k in adj_list:
+            if k not in visited:
+                q.append(k)
+                visited.add(k)
+                
+    return result
+
+            
+def bfs_zigzag(g, n):
+    # non-recursive implementation
+
+    if not g.contains(n):
+        raise GraphException("node %s not in graph" % n)
+    
+    q = collections.deque()
+    visited = set()
+
+    q.append(n)
+    visited.add(n)
+    result = str(n)
+
+    while len(q) > 0:
+
+        front = q.popleft()
+        
+        # enqueue all unvisited adj_list
+        adj_list = g.neighbors(front)
+        for k in adj_list:
+            if k not in visited:
+                q.append(k)
+                visited.add(k)
+                result += str(k)
+
+    return result
+
+            
---
+++ testgraph.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python
+
+import unittest
+import graph
+
+class TestNode(unittest.TestCase):
+
+    def test_init(self):
+        n = graph.Node('a')
+        self.assertEqual('a', str(n))
+        self.assertEqual('a', n.label)
+        self.assertEqual('Node(a)', repr(n))
+        
+    def test_readonly(self):
+        n = graph.Node('a')
+        with self.assertRaises(AttributeError):
+            n.label = 'b'
+
+    def test_equals(self):
+
+        m = graph.Node('aoeu')
+        n = graph.Node('aoeu')
+
+        self.assertEqual(n, n)
+        self.assertEqual(m, n)
+        
+        q = graph.Node('ueoa')
+        self.assertNotEqual(q, m)
+
+
+class TestGraph(unittest.TestCase):
+
+    def test_basic(self):
+        g = graph.Graph()
+        self.assertEqual(0, len(g.adj_list))
+        self.assertEqual(0, len(g.nodes()))
+
+    def test_addNode(self):
+        g = graph.Graph()
+        n1 = graph.Node('a')
+        
+        g.addNode(n1)
+        self.assertEqual(1, len(g.nodes()))
+        
+        g.addNode(n1)
+        self.assertEqual(1, len(g.nodes()))
+
+        n2 = graph.Node('a')
+
+        g.addNode(n2)
+        self.assertEqual(1, len(g.nodes()))
+
+        n3 = graph.Node('b')
+        g.addNode(n3)
+        self.assertEqual(2, len(g.nodes()))
+
+        self.assertTrue(g.contains(n1))
+        self.assertTrue(g.contains(n2))
+        self.assertTrue(g.contains(n3))
+
+        n4 = graph.Node('notMe')
+        self.assertFalse(g.contains(n4))
+        
+    def test_addEdge(self):
+
+        g = graph.Graph()
+        a = graph.Node('a')
+        b = graph.Node('b')
+        with self.assertRaises(Exception):
+            g.addEdge(a, b)
+
+        g.addNode(a)
+        with self.assertRaises(Exception):
+            g.addEdge(a, b)
+
+        g.addNode(b)
+        g.addEdge(a, b)
+
+    def test_sorting(self):
+        # make sure that a node's neighbors are
+        # presented in sorted order.
+
+        gr = graph.Graph()
+        a = graph.Node('a')
+        b = graph.Node('b')
+        c = graph.Node('c')
+        d = graph.Node('d')
+        e = graph.Node('e')
+        f = graph.Node('f')
+        g = graph.Node('g')
+        h = graph.Node('h')
+
+        gr.addNode(a)
+        gr.addNode(b)
+        gr.addNode(c)
+        gr.addNode(d)
+        gr.addNode(e)
+        gr.addNode(f)
+        gr.addNode(g)
+        gr.addNode(h)
+        
+        gr.addEdge(a, h)
+        gr.addEdge(a, g)
+        gr.addEdge(a, f)
+        gr.addEdge(a, e)
+        gr.addEdge(a, d)
+        gr.addEdge(a, c)
+        gr.addEdge(a, b)
+
+        k = ""
+        for n in gr.neighbors(a):
+            k += str(n)
+
+        self.assertEqual('bcdefgh', k)
+
+    def test_dfs(self):
+
+        # cf. https://www.youtube.com/watch?v=zLZhSSXAwxI
+
+        gr = graph.Graph()
+        a = graph.Node('a')
+        b = graph.Node('b')
+        c = graph.Node('c')
+        d = graph.Node('d')
+        e = graph.Node('e')
+        f = graph.Node('f')
+        g = graph.Node('g')
+        h = graph.Node('h')
+
+        gr.addNode(a)
+        gr.addNode(b)
+        gr.addNode(c)
+        gr.addNode(d)
+        gr.addNode(e)
+        gr.addNode(f)
+        gr.addNode(g)
+        gr.addNode(h)
+        
+        gr.addEdge(a, b)
+        gr.addEdge(a, g)
+        gr.addEdge(a, d)
+
+        gr.addEdge(b, e)
+        gr.addEdge(b, f)
+
+        gr.addEdge(c, f)
+        gr.addEdge(c, h)
+
+        gr.addEdge(d, f)
+        gr.addEdge(e, g)
+
+        outsider = graph.Node('mr_lonely')
+        
+        with self.assertRaises(graph.GraphException):
+            graph.dfs(gr, outsider)
+
+        r = graph.dfs(gr, a)
+        self.assertEqual('abegfchd', r)
+
+    def test_bfs_simple(self):
+
+        # graph with one vertex and no edges
+        # graph with one vertex and one (loop) edge
+
+        gr = graph.Graph()
+        a = graph.Node('a')
+        
+        outsider = graph.Node('mr_lonely')
+        
+        with self.assertRaises(graph.GraphException):
+            graph.bfs(gr, outsider)
+
+        gr.addNode(a)
+        gr.addEdge(a, a)
+
+        graph.bfs(gr, a)
+        
+
+    def test_bfs(self):
+
+        gr = graph.Graph()
+        a = graph.Node('a')
+        b = graph.Node('b')
+        c = graph.Node('c')
+        d = graph.Node('d')
+        e = graph.Node('e')
+        f = graph.Node('f')
+        g = graph.Node('g')
+        h = graph.Node('h')
+
+        gr.addNode(a)
+        gr.addNode(b)
+        gr.addNode(c)
+        gr.addNode(d)
+        gr.addNode(e)
+        gr.addNode(f)
+        gr.addNode(g)
+        gr.addNode(h)
+        
+        gr.addEdge(a, b)
+        gr.addEdge(a, g)
+        gr.addEdge(a, d)
+
+        gr.addEdge(b, e)
+        gr.addEdge(b, f)
+
+        gr.addEdge(c, f)
+        gr.addEdge(c, h)
+
+        gr.addEdge(d, f)
+        gr.addEdge(e, g)
+
+        r = graph.bfs(gr, a)
+        self.assertEqual('abdgefch', r)
+
+    def test_zigzag(self):
+        gr = graph.Graph()
+        a = graph.Node('a')
+        b = graph.Node('b')
+        c = graph.Node('c')
+        d = graph.Node('d')
+        e = graph.Node('e')
+        f = graph.Node('f')
+        g = graph.Node('g')
+        h = graph.Node('h')
+        i = graph.Node('i')
+        j = graph.Node('j')
+
+        gr.addNode(a)
+        gr.addNode(b)
+        gr.addNode(c)
+        gr.addNode(d)
+        gr.addNode(e)
+        gr.addNode(f)
+        gr.addNode(g)
+        gr.addNode(h)
+        gr.addNode(i)
+        gr.addNode(j)
+
+        gr.addEdge(a, b)
+        gr.addEdge(a, c)
+
+        gr.addEdge(b, d)
+        gr.addEdge(b, e)
+
+        gr.addEdge(c, f)
+
+        gr.addEdge(d, g)
+        gr.addEdge(d, h)
+
+        gr.addEdge(f, i)
+        gr.addEdge(f, j)
+
+        r = graph.bfs_zigzag(gr, a)
+        print r
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
